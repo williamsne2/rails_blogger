92b09964b1030d06fe8294122346170692d80e02
williamsne2 <williamsne2@gmail.com>
2019-06-14 17:02:19 -0500

    create article page inserted

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,4 +6,12 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(params[:article])
+        @article.save
+        redirect_to article_path(@article)
+    end
 end
